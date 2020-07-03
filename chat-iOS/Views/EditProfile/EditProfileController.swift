491dccff4de4cde6fedc98456b95392bea88500b
Aki Kuratani <aki112553@gmail.com>
2020-07-03 14:12:24 +0900

    プロフィール編集画面の各種ファイル追加しました。

---
+++ chat-iOS/Views/EditProfile/EditProfileController.swift
@@ -0,0 +1,25 @@
+//
+//  EditProfileController.swift
+//  chat-iOS
+//
+//  Created by 倉谷　明希 on 2020/07/03.
+//
+
+import UIKit
+
+final class EditProfileViewController: UIViewController {
+    private var presenter: EditProfileViewPresenterProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func inject(with presenter: EditProfileViewPresenterProtocol) {
+        self.presenter = presenter
+        self.presenter.view = self
+    }
+}
+
+extension EditProfileViewController: EditProfileViewPresenterOutput {
+    
+}
---
+++ chat-iOS/Views/EditProfile/EditProfileModel.swift
@@ -0,0 +1,19 @@
+//
+//  EditProfileModel.swift
+//  chat-iOS
+//
+//  Created by 倉谷　明希 on 2020/07/03.
+//
+
+protocol EditProfileModelProtocol {
+    var presenter: EditProfileModelOutput! { get set }
+}
+
+protocol EditProfileModelOutput {
+    
+}
+
+final class EditProfileModel: EditProfileModelProtocol {
+    var presenter: EditProfileModelOutput!
+}
+
