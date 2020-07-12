fe7c3d9dd6c32a3d92f3582f96092e68062e0c99
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-12 21:24:40 +0900

    `fetchTransScript`の処理を`reduce`ではなく`flatmap`で行うように変更。それに伴って関数`decodeTransScript()`を削除した。

--- chat-iOS/Views/Chats/ChatsViewModel.swift
+++ chat-iOS/Views/Chats/ChatsViewModel.swift
@@ -40,17 +40,6 @@ final class ChatsViewModel: ChatsViewModelProtocol {
         self.firestore.settings = settings
     }
     
-    /// documentDataをTransScriptにデコードする関数
-    /// - Parameter documentData: FirestoreのDocumentData
-    /// - Returns: デコードしたやつ
-    private func decodeTransScript(documentData: [String: Any]) -> Transcript {
-        do {
-            return try Firestore.Decoder().decode(Transcript.self, from: documentData)
-        } catch {
-            fatalError("Error decoding firestore data")
-        }
-    }
-    
     func fetchTransScript() {
         //TODO:- v1とはか切り出す。また,roomIDは引数で持ってくる。このroomIDはデバック用whereFieldを使って時系列順に取り出す
         let roomID = "gjqF2hDA0SAV8sad15jU"
@@ -60,14 +49,13 @@ final class ChatsViewModel: ChatsViewModelProtocol {
                 return
             }
             
-            guard let snapshot = documentSnapshot else { return }
-                        
-            let transcripts = snapshot.documents.reduce([Transcript]()) { (array, document) -> [Transcript] in
-                var array = array
-                guard document.exists else { return array }
-                
-                array.append(self.decodeTransScript(documentData: document.data()))
-                return array
+            guard let documents = documentSnapshot?.documents else {
+                print("The document doesn't exist.")
+                return
+            }
+            
+            let transcripts = documents.compactMap { queryDocumentSnapshot -> Transcript? in
+                return try? queryDocumentSnapshot.data(as: Transcript.self)
             }
             
             self.presenter.successFetchTransScript(transScripts: transcripts)
