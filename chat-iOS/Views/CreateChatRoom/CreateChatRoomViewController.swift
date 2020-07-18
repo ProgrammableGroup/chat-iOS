ae1492c749a10585c01edc2926ade85d93245f84
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-18 12:42:56 +0900

    CreateChatRoomViewControllerのセットアップを行った

--- chat-iOS/Views/CreateChatRoom/CreateChatRoomViewController.swift
+++ chat-iOS/Views/CreateChatRoom/CreateChatRoomViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 final class CreateChatRoomViewController: UIViewController {
     private var presenter: CreateChatRoomViewPresenterProtocol!
     
+    @IBOutlet weak var userNameSearchBar: UISearchBar!
     @IBOutlet weak var serchUserTableview: UITableView!
     @IBOutlet weak var selectedUserCollectionView: UICollectionView!
+    
+    var searchedUsers: [User] = Array()
+    var selectedUsers: [User] = Array()
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupUserSearchBar()
+        self.setupSerchUserTableview()
+        self.setupSelectedUserCollectionView()
+    }
+    
+    private func setupUserSearchBar() {
+        self.userNameSearchBar.placeholder = "Search by name"
+        self.userNameSearchBar.delegate = self
+    }
+    
+    private func setupSerchUserTableview() {
+        self.serchUserTableview.rowHeight = 75
+        self.serchUserTableview.delegate = self
+        self.serchUserTableview.dataSource = self
+        self.serchUserTableview.tableFooterView = UIView()
+    }
+    
+    private func setupSelectedUserCollectionView() {
+        self.selectedUserCollectionView.isHidden = true
     }
     
     func inject(with presenter: CreateChatRoomViewPresenterProtocol) {
@@ -25,4 +49,31 @@ final class CreateChatRoomViewController: UIViewController {
 
 extension CreateChatRoomViewController: CreateChatRoomViewPresenterOutput {
     
+}
+
+extension CreateChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.searchedUsers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+
+}
+
+extension CreateChatRoomViewController: UISearchBarDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        guard self.userNameSearchBar.isFirstResponder else { return }
+        self.userNameSearchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchBarText = searchBar.text else { return }
+        guard self.userNameSearchBar.isFirstResponder else { return }
+        self.userNameSearchBar.resignFirstResponder()
+        
+        print("searchText = \(searchBarText)")
+    }
 }
