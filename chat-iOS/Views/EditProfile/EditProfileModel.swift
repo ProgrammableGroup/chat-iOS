1930ca176be07d923e8637667c0a5333767ddf16
Aki Kuratani <aki112553@gmail.com>
2020-07-20 19:45:18 +0900

    エラー出たからコメントアウトした

--- chat-iOS/Views/EditProfile/EditProfileModel.swift
+++ chat-iOS/Views/EditProfile/EditProfileModel.swift
@@ -19,24 +19,24 @@ final class EditProfileModel: EditProfileModelProtocol {
     var presenter: EditProfileModelOutput!
     
     func saveProfile() {
-        let storage = Storage.storage().reference(forURL: "gs://mapapp6-bf5a1.appspot.com")
-        let imageRef = storage.child("profileImage").child("\(user.uid).jpeg")
-        var ProfileImageData: Data = Data()
-        if imageView.image != nil {
-            
-        //画像を圧縮
-        ProfileImageData = (imageView.image?.jpegData(compressionQuality: 0.01))!
-            
-        }
-        imageRef.putData(ProfileImageData, metadata: nil) { (metaData, error) in
-            
-            //エラーであれば
-            if error != nil {
-                
-                print(error.debugDescription)
-                return  //これより下にはいかないreturn
-            }
-        }
+//        let storage = Storage.storage().reference(forURL: "gs://mapapp6-bf5a1.appspot.com")
+//        let imageRef = storage.child("profileImage").child("\(user.uid).jpeg")
+//        var ProfileImageData: Data = Data()
+//        if imageView.image != nil {
+//
+//        //画像を圧縮
+//        ProfileImageData = (imageView.image?.jpegData(compressionQuality: 0.01))!
+//
+//        }
+//        imageRef.putData(ProfileImageData, metadata: nil) { (metaData, error) in
+//
+//            //エラーであれば
+//            if error != nil {
+//
+//                print(error.debugDescription)
+//                return  //これより下にはいかないreturn
+//            }
+//        }
         
         
         
