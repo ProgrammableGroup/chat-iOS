0296a9cdc6bdb31a1d8dcf7f40b0ac0d2a63a7d3
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-07-22 16:34:46 +0900

    Roomに新しくプロパティを追加

--- chat-iOS/Model/Room.swift
+++ chat-iOS/Model/Room.swift
@@ -4,10 +4,13 @@
 //
 //  Created by 戸高新也 on 2020/07/04.
 //
+import Firebase
 
 struct Room: Codable {
     let name: String?
     let thumbnailImageURL: String?
     let members: [String]
     let message: String
+    @DocumentID var id: String? 
+    @ServerTimestamp var updateAt: Timestamp?
 }
